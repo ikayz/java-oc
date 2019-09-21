fbe2112c67772873b90adacdb07d23631dfb1263
Isaac Miti <ikayz@live.com>
2019-09-21 19:22:59 +0200

    Add array to main class

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,11 @@ public class Main {
         theHobbit.author =  "J.R.R Tolkien";
         theHobbit.numberOfPages = 430;
 
+        String[] names = new String[4];
+        names = new String[]{"Frodo", "Sam", "Merry", "Pippin", "Gandolf"};
+
         System.out.println("The book name is " + theHobbit.title + " written by " + theHobbit.author + " and has " +
                 theHobbit.numberOfPages + " pages.");
+        System.out.println(names[4]);
     }
 }
