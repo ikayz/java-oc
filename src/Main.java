a9e9eaa6e5d53e34ffbeac62c859f5b4789bce3d
Isaac Miti <ikayz@live.com>
2019-09-21 13:12:33 +0200

    Assign values to object instance

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import java.awt.print.Book;
 
 public class Main {
     public static void main(String[] args) {
-        Book theHobbit = new Book();
+        Books theHobbit = new Books();
+        theHobbit.title = "The Hobbit: An Unexpected Journey";
+        theHobbit.author =  "J.R.R Tolkien";
+        theHobbit.numberOfPages = 430;
+
+        System.out.println("The book name is " + theHobbit.title + "written by " + theHobbit.author + " and has " +
+                theHobbit.numberOfPages + " pages.");
     }
 }
