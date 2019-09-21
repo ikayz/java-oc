e25422304fa47b12451a58e0e54e23ca5129256b
Isaac Miti <ikayz@live.com>
2019-09-21 20:01:26 +0200

    Add List type and assign variables

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +12,14 @@ public class Main {
         String[] names = new String[4];
         names = new String[]{"Frodo", "Sam", "Merry", "Pippin", "Gandolf"};
 
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(23);
+        myList.add(1, 9);
+        myList.add(2,91);
+
         System.out.println("The book name is " + theHobbit.title + " written by " + theHobbit.author + " and has " +
                 theHobbit.numberOfPages + " pages.");
         System.out.println(names[4]);
+        System.out.println(myList);
     }
 }
