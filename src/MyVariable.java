1025ac6421ad14a5155c3b34897b5eba8dc9b4c8
Isaac Miti <ikayz@live.com>
2020-06-29 12:26:08 +0200

    Add shortcut class

---
+++ src/MyVariable.java
@@ -0,0 +1,10 @@
+public class MyVariable {
+    public static void main(String[] args) {
+        int ongoingAllowance = 200;
+        int savings = 3000;
+
+        int bonusAllowance = 500;
+
+        System.out.println("You have an ongoing allowance of " + ongoingAllowance + "\nYour savings amount is " + savings + "\nYou have a bonus allowance of" + bonusAllowance);
+    }
+}
---
+++ src/ShortcutMath.java
@@ -0,0 +1,9 @@
+public class ShortcutMath {
+    public static void main(String[] args) {
+        int ongoingAllowance = 200;
+        int savings = 3000;
+
+        savings += 500;
+        System.out.println("Your savings amount is: " + savings);
+    }
+}
