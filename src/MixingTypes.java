e9f145e4f565e0d468b2755f715c941374a4f00d
Isaac Miti <ikayz@live.com>
2020-07-06 13:06:19 +0200

    Add mixing types

---
+++ src/MixingTypes.java
@@ -0,0 +1,12 @@
+public class MixingTypes {
+    public static void main(String[] args) {
+        int numerator = 10;
+        int denominator = 4;
+
+        // Change the statement below so that answer holds a decimal value
+        double answer = numerator/(double)denominator;
+
+        //Print the result
+        System.out.println("" + numerator + "/" + denominator + "=" + answer);
+    }
+}
