1344915b528184bf0a6b1a2556c4e0639d1c170e
Isaac Miti <ikayz@live.com>
2020-07-06 13:14:40 +0200

    Add concatenate string class

---
+++ src/ConcatenateStrings.java
@@ -0,0 +1,13 @@
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        String firstString = "dogs";
+        String secondString = "cats";
+
+        // Create a new string variable called animals that says 'I love both dogs and cats!'
+        String animals = "I love both " + firstString +  " and " + secondString;
+
+        // print the result
+        System.out.println(animals);
+
+    }
+}
