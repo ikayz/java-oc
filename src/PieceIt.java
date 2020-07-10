2f8f37b8770d0767a45a7b4b9d14504e4fc6b41f
Isaac Miti <ikayz@live.com>
2020-07-10 16:27:22 +0200

    Add Piece it class

---
+++ src/PieceIt.java
@@ -0,0 +1,13 @@
+public class PieceIt {
+    public static void main(String[] args) {
+        String name = "Isaac Ike";
+        int age = 23;
+        int dateOfBirth = 1997;
+        String town = "Lusaka";
+        String favouritePet = "Cat";
+        String petName = "Light";
+
+        System.out.println("My name is " + name + " I am " + age + " years old, born in " + dateOfBirth + " and..."
+                + " My favourite type of pet is a " + favouritePet + " which goes by " + petName + " in " + town);
+    }
+}
