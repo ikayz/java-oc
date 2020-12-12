d7a1b452fc99a070e4f8616d2d40e4851c11d0b2
Isaac Miti <ikayz@live.com>
2020-12-12 19:16:36 +0200

    Replace some guests

--- src/GuestsList.java
+++ src/GuestsList.java
@@ -2,5 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GuestsList {
-    List<String> guests = new ArrayList<String>();
+    public static void main(String[] args) {
+        List<String> guests = new ArrayList<String>();
+
+        guests.add("John");
+        guests.add("Suwi");
+        guests.add("Maria");
+
+        System.out.println(guests.size());
+
+        // Replace two guests
+        guests.set(1, "Chimba");
+        guests.set(0, "Yo");
+
+        System.out.println("The guests are: ");
+        for (String guest:guests) {
+            System.out.println(guest);
+        }
+    }
 }
