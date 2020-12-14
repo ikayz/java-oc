f6ca929175f77e47787a3fc154c650e1a21928ea
Isaac Miti <ikayz@live.com>
2020-12-14 14:52:21 +0200

    Add and remove ingredients from the set

--- src/IngredientsSet.java
+++ src/IngredientsSet.java
@@ -1,5 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class IngredientsSet {
     public static void main(String[] args) {
+        Set<String> ingredients = new HashSet<>();
 
+        ingredients.add("Sugar");
+        ingredients.add("Milk");
+        ingredients.add("Chocolate");
+        ingredients.add("Vanilla");
+
+        // Remove from set
+        ingredients.remove("Milk");
+
+        // Replace in set
+        ingredients.add("Butter");
+
+        System.out.println("Here is a list of your ingredients: ");
+        for (String ingredient:ingredients) {
+            System.out.println(ingredient);
+        }
     }
 }
